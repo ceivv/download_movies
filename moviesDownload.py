921a0cf5b1014157f9d082a458b68143dd0a9a44
bassem <bassem.jadoui@esprit.tn>
2022-05-21 18:38:15 +0100

    add auto install BeautifulSoup4 if not exist ✅✅

--- moviesDownload.py
+++ moviesDownload.py
@@ -1,10 +1,16 @@
-	#Downlaod movies from console for Super lazy Lads
-	#(no egybest popup ads sh*t no more)
-	#Ceiv 21/05/2022
-	#enjoy :)
-
+# Downlaod movies from console for Super lazy Lads
+# (no egybest popup ads sh*t no more)
+# Ceiv 21/05/2022
+# enjoy :)
+import os
 import requests
-from bs4 import BeautifulSoup
+
+try:
+    from bs4 import BeautifulSoup
+except ImportError:
+    print("Trying to Install required module: BeautifulSoup\n")
+    os.system('python -m pip install BeautifulSoup4')
+
 import webbrowser
 
 moviesInfo = []
@@ -13,64 +19,57 @@ movies = []
 
 # hadnling missing search results
 while True:
-	# avoid empty input
-	while True :
-		movieName = input("Movie name >")
-		if movieName != "":
-			break
-	#forming search link
-	x = movieName.split()
-	name = "+".join(x)
-	result = requests.get("https://mycima.cloud/search/"+ name)
-	src = result.content
-	soup = BeautifulSoup(src, "html.parser")
+    # avoid empty input
+    while True:
+        movieName = input("Movie name >")
+        if movieName != "":
+            break
+    # forming search link
+    x = movieName.split()
+    name = "+".join(x)
+    result = requests.get("https://mycima.cloud/search/" + name)
+    src = result.content
+    soup = BeautifulSoup(src, "html.parser")
 
-	#getting search results
-	moviesInfo = soup.find_all("div",{"class":"Thumb--GridItem"})
-	if len(moviesInfo)>0:
-		break
-	print("No such a movie :( try again !")
+    # getting search results
+    moviesInfo = soup.find_all("div", {"class": "Thumb--GridItem"})
+    if len(moviesInfo) > 0:
+        break
+    print("No such a movie :( try again !")
 
 # handling single or multiple serach results
-if len(moviesInfo) > 1 :
-	for y in range(len(moviesInfo)):
-		print(str(y)+" : "+ moviesInfo[y].text)
+if len(moviesInfo) > 1:
+    for y in range(len(moviesInfo)):
+        print(str(y) + " : " + moviesInfo[y].text)
 
-	#handling wrong or non mumerical input
-	while(1):
-		choice = int(input("choose movie: "))
-		if choice in range(len(moviesInfo)):
-			break
-	chosenMovie = moviesInfo[int(choice)]
+    # handling wrong or non mumerical input
+    while (1):
+        choice = int(input("choose movie: "))
+        if choice in range(len(moviesInfo)):
+            break
+    chosenMovie = moviesInfo[int(choice)]
 
-	#movie download page  multiple results
-	link = chosenMovie.find("a").attrs['href']
-else : 
-	#movie download page single result
-	link = moviesInfo[0].find("a").attrs['href']
+    # movie download page  multiple results
+    link = chosenMovie.find("a").attrs['href']
+else:
+    # movie download page single result
+    link = moviesInfo[0].find("a").attrs['href']
 
 # opening movie link one
-result = requests.get (link)
+result = requests.get(link)
 src = result.content
 soup = BeautifulSoup(src, "html.parser")
-movies = soup.find_all("a",{"class":"hoverable activable"})
+movies = soup.find_all("a", {"class": "hoverable activable"})
 
-#getting movies links for each movie 
-for movie in movies :
-	if "upbam" in movie['href']:
-		moviesLinks.append(movie['href'])
+# getting movies links for each movie
+for movie in movies:
+    if "upbam" in movie['href']:
+        moviesLinks.append(movie['href'])
 
-#listing movie links according to quality
+# listing movie links according to quality
 for i in range(len(moviesLinks)):
-	print(str(i)+" : "+ str(moviesLinks[i]))
+    print(str(i) + " : " + str(moviesLinks[i]))
 quality = input("choose quality > ")
 
-#opening the download popup
+# opening the download popup
 webbrowser.open(moviesLinks[int(quality)])
-
-
-
-
-
-
-
